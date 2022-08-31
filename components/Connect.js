ff5e4535248d5b2461fbfe6d252e9b2e0356d683
web3Codess <vercelploy@gmail.com>
2022-08-31 21:36:17 +0300

    ..

--- components/Connect.js
+++ components/Connect.js
@@ -4,14 +4,18 @@ import Link from 'next/link';
 export default function Connect() {
   return (
     <>
+
       <div className="flex items-center justify-center h-[120vh] w-10/12 m-auto lg:w-11/12">
         <div className="grid items-center justify-items-center text-center max-w-3xl">
             <h1 className="">HELPING COMPANIES SUCCEED IN WEB3 WORLD</h1>
           
             <p className="text-base md:text-sm text-slate-500 my-5
             ">CLICK TO DM US FOR A SERVICE 👇</p>
-<div className='grid gap-y-5'>
-               <div >
+
+
+              <div className='grid gap-y-5'>
+
+               <div>
                <button type='button' className=" bg-blue-600 px-7 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all">
                 <Link href={`https://t.me/metavass`}>NFT MINTING SITE 
                 GENERATE 10K NFT COLLECTION AND SELL</Link>
@@ -25,66 +29,91 @@ export default function Connect() {
                 </div>
 
 
+                <div>
 
                 <button type='button' className="bg-blue-600 px-7 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all ">
                 <Link href={`https://t.me/metavass`}>NFT MARKET PLACE LIKE OPENSEA</Link>
                 </button>
+                </div>
 
+                <div>
 
                 <button type='button' className="bg-blue-600 px-7 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all">
                 <Link href={`https://t.me/metavass`}>NFT SMART CONTRACT</Link>
                 </button>
+                </div>
 
-                <button type='button' className="bg-blue-600 px-7 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all">
+
+                <div>
+                <button type='button' className="bg-blue-600 px-7
+                 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all">
                 <Link href={`https://t.me/metavass`}>CRYPTO COIN DEVELOPMENT </Link>
                 </button>
+                </div>
 
+
+                <div>
                 <button type='button' className="bg-blue-600 px-7 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all">
                 <Link href={`https://t.me/metavass`}>HOW TO FILTER SPAM NFTS </Link>
                 </button>
+                </div>
+
+                <div>
 
                 <button type='button' className="bg-blue-600 px-7 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all">
                 <Link href={`https://t.me/metavass`}>HOW TO SEND A PRIVATE TRANSACTION ON ETHEREUM </Link>
                 </button>
+                </div>
+                <div>
 
                 <button type='button' className="bg-blue-600 px-7 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all">
                 <Link href={`https://t.me/metavass`}>HOW TO CANCEL A TRANSACTION ON ETHEREUM </Link>
                 </button>
+                </div>
 
+                <div>
 
                 <button type='button' className="bg-blue-600 px-7 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all">
                 <Link href={`https://t.me/metavass`}>HOW TO CHECK OWNER OF AN NFT</Link>
                 </button>
+                </div>
 
-
+                <div>
                 <button type='button' className="bg-blue-600 px-7 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all">
                 <Link href={`https://t.me/metavass`}>HOW TO GET ALL NFTS OWNED BY AN ADDRESS </Link>
                 </button>
+                </div>
 
 
              
 
-
+                <div>
                 <button type='button' className="bg-blue-600 px-7 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all">
                 <Link href={`https://t.me/metavass`}>HOW TO CREATE WHALE ALERT BOTS </Link>
                 </button>
+                </div>
 
-
+                <div>
                 <button type='button' className="bg-blue-600 px-7 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all">
                 <Link href={`https://t.me/metavass`}>CRYPTO EXCHANGE WEBSITE </Link>
                 </button>
+                </div>
 
-
+<div>
                 <button type='button' className="bg-blue-600 px-7 py-1.5 text-lg md:text-sm rounded-md shadow-lg shadow-blue-600 active:scale-90 hover:translate-y-2 transition-all">
                 <Link href={`https://t.me/metavass`}>WEB3 CONSULTATION </Link>
                 </button>
+                </div>
 
 </div>
-        </div>
-      </div>
-    </>
+</div>
+</div>
+
+
+        
+      </>
+    
   );
 };
 
 
-{/* <Link href={`https://testnets.opensea.io/collection/100-ethicon-v2`}><a target={`_blank`} className="text-blue-500 underline">View On Opensea</a></Link> */}
